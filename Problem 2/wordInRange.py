1986a4f1af95b3349e7e1be6a19a84a42fb90740
Randomsnowman2005 <mohdali.fakhoury@gmail.com>
2024-03-14 14:24:09 -0500

    finished prob2

--- Problem 2/wordInRange.py
+++ Problem 2/wordInRange.py
@@ -1,6 +1,17 @@
 #Mohamad Ali Fakhoury Thurs@2pm
 def wordInRange():
-    #Type your code here
+    file = open('./Problem 2/input1.txt','r')
+    min = 'ammoniated'
+    max = 'millennium'
+    x= file.readline()
+    lines = file.readlines()
+    for line in lines:
+        lin = line.strip('\n')
+        if (lin >= min) and (lin <= max):
+            print(f'{line.strip()} - in range')
+        else:
+            print(f'{line.strip()} - not in range') 
+    file.close
     return
 if __name__ == '__main__':
     wordInRange()
--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -2,6 +2,13 @@
 def courseGrade():
     
     # TODO: Declare any necessary variables here. 
+    fil = open(f'./Problem 3/{str(input())}','r')
+    x = str(fil.readline())
+    while x != '':
+        cut = x[-1,-10]
+        print (cut)
+        x = str(fil.readline)
+        
       
       
     # TODO: Read a file name from the user and read the tsv file here. 
