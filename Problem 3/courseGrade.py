bb70f188066cdfb21aa07d7b994a512779389465
Randomsnowman2005 <mohdali.fakhoury@gmail.com>
2024-03-15 17:51:23 -0500

    prob 3 Done

--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -4,13 +4,12 @@ def courseGrade():
     # TODO: Declare any necessary variables here. 
     temp = str(input())
     fil = open(temp,'r')
-    end = open('report.txt','w')
     if temp == "./Problem 3/StudentInfo.tsv":
-         end = open('report.txt','w')
+         end = open('./Problem 3/report.txt','w')
     elif temp == "./Problem 3/StudentInfo1.tsv":
-         end = open('report1.txt','w')
+         end = open('./Problem 3/report1.txt','w')
     else:
-         end = open('report2.txt','w')
+         end = open('./Problem 3/report2.txt','w')
     x = str(fil.readline())
     cut=''
     lis = []
@@ -19,12 +18,15 @@ def courseGrade():
     lett = []
     name=[]
     y=0
+    mid1 = 0
+    mid2 = 0
+    fina = 0
     cut2=''
     # TODO: Read a file name from the user and read the tsv file here. 
     while x != '':
-      cut=cut + x[-9:-1]
+      cut=cut + x[-9:]
       leg = len(x)
-      cut2 = cut2 + x[:leg-10]
+      cut2 = cut2 + x[:leg-9]
       lis.append(cut.split(  ))
       nem.append(cut2.split())
       x = str(fil.readline())
@@ -46,8 +48,15 @@ def courseGrade():
            lett.append('D')
         else:
            lett.append('F')
-        end.write(f'{name[0]}   {name[1]}   {grade[0]}  {grade[1]}  {grade[2]}  {lett[y]}')
+        end.write(f'{name[0]}\t{name[1]}\t{grade[0]}\t{grade[1]}\t{grade[2]}\t{lett[y]}\n')
+        mid1 = mid1 + int(grade[0])
+        mid2 = mid2 + int(grade[1])
+        fina = fina + int(grade[2])
         y=y+1
+    mid1 = mid1/len(lis)
+    mid2 = mid2/len(lis)
+    fina= fina/len(lis)
+    end.write(f'Averages: midterm1 {mid1:.2f}, midterm2 {mid2:.2f}, final {fina:.2f}')
     end.close
     return
 
