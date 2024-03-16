5379c926221756b1154365dc3ba47ae3961e1dff
Randomsnowman2005 <mohdali.fakhoury@gmail.com>
2024-03-15 20:44:08 -0500

    prob 4 done

--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -1,7 +1,19 @@
 #Mohamad Ali Fakhoury Thurs@2pm
 def fileNameChange():
-    #type your code here
-
+    fil = open(str(input()),'r')
+    x= str(fil.readline())
+    cut=[]
+    newl=''
+    if x != '':
+        while x != '':
+            cut = x.split('_')
+            newl = str(cut[0]) + '_info.txt'
+            print(f'{newl}\n')
+            x=fil.readline()
+    else:
+        fil.close
+        return
+    fil.close
     return
 
 if __name__ == '__main__':
